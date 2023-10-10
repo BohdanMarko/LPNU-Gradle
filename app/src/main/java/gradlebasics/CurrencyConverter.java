32f413d0d239159f8cfec662c8c89782a0d0991a
Bohdan Marko <bohdan.marko.228@gmail.com>
2023-10-10 17:56:08 +0300

    create perfect unit tests

--- app/src/main/java/gradlebasics/CurrencyConverter.java
+++ app/src/main/java/gradlebasics/CurrencyConverter.java
@@ -6,18 +6,30 @@ public class CurrencyConverter {
     private static final double EUR_COEFFICIENT = 38.6;
 
     public static double convertUAHtoUSD(double uah) {
+        if (uah < 0) {
+            throw new IllegalArgumentException("Amount of money cannot be negative");
+        }
         return uah / USD_COEFFICIENT;
     }
 
     public static double convertUSDtoUAH(double usd) {
+        if (usd < 0) {
+            throw new IllegalArgumentException("Amount of money cannot be negative");
+        }
         return usd * USD_COEFFICIENT;
     }
 
     public static double convertUAHtoEUR(double uah) {
+        if (uah < 0) {
+            throw new IllegalArgumentException("Amount of money cannot be negative");
+        }
         return uah / EUR_COEFFICIENT;
     }
 
     public static double convertEURtoUAH(double eur) {
+        if (eur < 0) {
+            throw new IllegalArgumentException("Amount of money cannot be negative");
+        }
         return eur * EUR_COEFFICIENT;
     }
 }
--- app/src/test/java/gradlebasics/AppTest.java
+++ app/src/test/java/gradlebasics/AppTest.java
@@ -4,9 +4,41 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        var expected = "Hello world.";
-        var actual = "Hello world.";
-        assertEquals(expected, actual);
+    @Test
+    public void testConvertUAHtoUSD() {
+        var expected = 1.0;
+        var actual = CurrencyConverter.convertUAHtoUSD(36.5);
+        assert actual == expected;
+    }
+
+    @Test
+    public void testConvertUSDtoUAH() {
+        var expected = 36.5;
+        var actual = CurrencyConverter.convertUSDtoUAH(1.0);
+        assert actual == expected;
+    }
+
+    @Test
+    public void testConvertUAHtoEUR() {
+        var expected = 1.0;
+        var actual = CurrencyConverter.convertUAHtoEUR(38.6);
+        assert actual == expected;
+    }
+
+    @Test
+    public void testConvertEURtoUAH() {
+        var expected = 38.6;
+        var actual = CurrencyConverter.convertEURtoUAH(1.0);
+        assert actual == expected;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertUAHtoUSDException() {
+        CurrencyConverter.convertUAHtoUSD(-1.0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertUSDtoUAHException() {
+        CurrencyConverter.convertUSDtoUAH(-1.0);
     }
 }
